995ba56dc6d3f968af97e854dfc1269b999bf4b7
jrizza88 <jetorres88@gmail.com>
2016-08-22 22:16:40 -0400

    updated server and have login info

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 //var myConnection = require('./config/connection');
 var exphbs = require('express-handlebars');
-//var bcrypt = require('bcrypt-nodejs');
-var orm = require('./config/orm');
+var bcrypt = require('bcrypt-nodejs');
+//var orm = require('./config/orm');
 var passport = require('passport');
 var session = require('express-session');
 // have to pass on a Store object on to the session
@@ -15,35 +15,101 @@ var SequelizeStore = require('connect-session-sequelize')(session.Store);
 var LocalStrategy = require('passport-local').Strategy;
 
 
-var db =require('./models/index.js').sequelize
+var db = require('./models/index.js').sequelize
 // this is used to sync the data
 db.sync();
 var User = require('./models').User;
 var app = express();
 
-  app.set('view engine', 'html') // will be handlebars or html
-  app.engine('html', exphbs({
-    defaultLayout: 'main'
-  }));// handlebars or html
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 passport.use('local', new LocalStrategy(
   function(username, password, done) {
     console.log("i work!")
-    User.findOne({where: {username: username}} ).then(function(username){
+    User.findOne({where: {username: username} } ).then(function(user){
       if (!user){
         return done(null, false);
       }
-      var realPass = user.password;
-      if (!user.username){
-        return done(null, false);
-      }
-      return done(null, user);
-  })
-    .catch(function(err){
-      throw err;
-    })
-  }));
-});
+           if (!user.username) {
+             return done(null, false);
+           }
+
+           bcrypt.compare(password, user.password, function(err, result) {
+             if (result) {
+               return done(null, false);
+             }
+           })
+
+           return done(null, user);
+         })
+         .catch(function(err) {
+           throw err;
+         })
+       }
+     ));
+
+     passport.serializeUser(function(user, cb) {
+       console.log("hi there",user.id)
+       cb(null, user.id);
+     });
+
+     passport.deserializeUser(function(id, cb) {
+       User.findOne( {where: {id: id} }).then(function(user) {
+         cb(null, user);
+       }).catch(function(err) {
+         if (err) {
+           return cb(err);
+         }
+       });
+     });
+
+     app.use(bodyParser.urlencoded({ extended: true }))
+     app.use(bodyParser.json())
+     app.use(cookieParser())
+     app.use(session({
+       secret: 'keyboard cat',
+       store: new SequelizeStore({
+         db: db
+       }),
+       resave: false,
+       saveUninitialized: true
+     }));
+     app.use(passport.initialize());
+     app.use(passport.session());
+
+     // ------------------------------------
+     // ROUTES
+     // ------------------------------------
+
+     app.get('/', function(req, res) {
+       if (req.user) {
+         res.render('home', { name: req.user.username});
+       } else {
+         res.redirect('/login');
+       }
+     })
+
+     app.get('/login', function(req, res) {
+       res.render('login');
+     })
+
+     app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'}));
+
+     //app.listen(8000)
+var PORT = process.env.PORT || 8000;
+
+     app.listen(PORT, function () {
+     	console.log('database operation on port: ' + PORT);
+     });
+
+    // console.log("database operationing on: ", PORT);
+
+
+
+
+
 
 // also want to give the option to login via Linkedin
 //passport.use(new LinkedInStrategy({
@@ -57,64 +123,3 @@ passport.use('local', new LocalStrategy(
 //    });
 //  }
 //));
-
-passport.serializeUser(function(user, cb){
-  console.log("hi there", user.id)
-  cb(null, user.id);
-});
-
-passpot.deserializeUser(function(id, cb){
-  User.findOne( {where: {id: id} }).then(function(user){
-    cb(null, user);
-  }).catch(function(err) {
-      if (err) {
-          return cb(err);
-      }
-    })
-})
-
-
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser())
-  app.use(session({
-    secret: 'keyboard cat',
-    store: new SequelizeStore({
-      db: db
-    }),
-    app.use(passport.initialize());
-    app.use(passport.session());
-  }))
-  app.use(bodyParser.text());
-  app.use(bodyParser.json({
-    type: 'application/vnd.api+json'
-  }));
-  app.use(methodOverride('_method'));
-  app.use(express.static(__dirname + '/public'));
-
-  app.listen(PORT, function () {
-  	console.log('Listening on port: ' + PORT);
-  });
-
-//ROUTES
-app.get('/', function(req,res){
-  if(req.user) {
-    console.log(req.user)
-    res.render('home', {name: req.user.username});
-  } else {
-    res.redirect('/login');
-		}
-})
-
-app.get('/login', function(req, res){
-  res.render('login');
-})
-
-app.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login'}));
-
-
-
-var PORT = process.env.PORT || 8000;
