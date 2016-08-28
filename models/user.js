1d167443e693ea05158b49189e07b66306c0a0e2
Ethan Favia <efavia215@live.com>
2016-08-28 12:11:39 -0400

    form posting application to database

--- models/user.js
+++ models/user.js
@@ -10,8 +10,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
-      }
+        }
     }
   });
   return User;
