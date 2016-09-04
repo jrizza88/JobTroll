0f1c09d4310eb8f8da24db7a513ace15f45376e0
Jared Thomas <jjthom87@yahoo.com>
2016-09-03 20:42:47 -0400

    change

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ db.sync();
 var User = models.User;
 var Application = models.application;
 
-// var app = express();
+var app = express();
 
 // process.env.JAWSDB_URL = 'mysql://mxr4sw3jewzv5rw5:njpx6ck71xghi844@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tm4gkhpdfar6y7j6';
 
